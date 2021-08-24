2bbce5a8e556a04d4d8172c6be57f2245a9c4b31
Charles Herriau <charles.herriau@axa.fr>
2021-08-24 21:35:31 +0200

    Adapt to new pandas module structure

--- Geocoding_utils/Geocode.py
+++ Geocoding_utils/Geocode.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-import pandas as pd
+from pandas.io.json import json_normalize
 
 from Geocoding_utils.config import *
 
@@ -19,6 +19,6 @@ def geocode(data_to_geocode,
     request_data = data_for_request.to_dict('records')
 
     response = requests.post('http://localhost:8088/geocode_file', json=json.dumps(request_data))
-    geocoded_data = pd.json_normalize(response.json()['data'])
+    geocoded_data = json_normalize(response.json()['data'])
     data_to_geocode['lon'], data_to_geocode['lat'] = geocoded_data['lon'].values, geocoded_data['lat'].values
     return data_to_geocode
